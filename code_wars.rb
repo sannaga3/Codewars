ca4fa72935aba672b196e8b77105e7311f1e08d6
sannaga3 <n.tetsuya02@gmail.com>
2021-11-14 20:06:49 +0900

    計68問

--- code_wars.rb
+++ code_wars.rb
@@ -1703,3 +1703,23 @@
 
 
 # --------------------------------------------------------------------------------------------------
+
+# no.68
+# 6kyu Encrypt this!
+
+# def encrypt_this(text)
+  # return "" if text == ""
+  # text.split(" ").map { |st| st.chars.first.ord.to_s + (st.size > 1 ? st.chars.last : "") + (st.size > 3 ? st.scan(/^..(.*).$/).first.first : "" ) + (st.size > 2 ? st.chars[1] : "") }.join(' ')
+# end
+
+# p encrypt_this("") == ""
+# p encrypt_this("A wise old owl lived in an oak") == "65 119esi 111dl 111lw 108dvei 105n 97n 111ka"
+# p encrypt_this("The more he saw the less he spoke") == "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp"
+# p encrypt_this("The less he spoke the more he heard") == "84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare"
+# p encrypt_this("Why can we not all be like that wise old bird") == "87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri"
+# p encrypt_this("Thank you Piotr for all your help") == "84kanh 121uo 80roti 102ro 97ll 121ruo 104ple"
+
+# text.gsub(/\b(\w)(\w)?(\w+?)?(\w)?\b/) { "#{$1.ord}#{$4}#{$3}#{$2}" } 一番簡単
+
+
+# --------------------------------------------------------------------------------------------------
